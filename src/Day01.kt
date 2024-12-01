4ef5a9dea141a58c6780ac681a48dafd00f5735f
Ural <vremennyi@rambler.ru>
2024-12-02 04:05:50 +0500

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,39 @@
+
+import java.util.PriorityQueue
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val l = PriorityQueue<Int>()
+        val r = PriorityQueue<Int>()
+        input.forEach { s ->
+            val (left, right) = s.split(Regex("\\s+"), 2).map(String::toInt)
+            l.add(left)
+            r.add(right)
+        }
+        var sum = 0
+        while (l.isNotEmpty() && r.isNotEmpty()) {
+            sum += abs(l.remove() - r.remove())
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val l = arrayListOf(input.size)
+        val counts = mutableMapOf<Int, Int>()
+        input.forEach { s ->
+            val (left, right) = s.split(Regex("\\s+"), 2).map(String::toInt)
+            l.add(left)
+            counts[right] = (counts[right] ?: 0) + 1
+        }
+        return l.sumOf { num -> num * (counts[num] ?: 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    val testInput = readInput("input/Day01_test")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("input/Day01")
     part1(input).println()
     part2(input).println()
 }
